d8986a316ddce9605bbdf36578784117ccad40a8
Yaar-Nuul <yaarnuulreech@gmail.com>
2024-07-19 23:12:46 +0300

    add files

---
+++ app/src/main/java/com/akirachix/phoenixapp/LeaderBoardItemActivity.kt
@@ -0,0 +1,17 @@
+package com.akirachix.phoenixapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.akirachix.phoenixapp.databinding.ActivityLeaderBoardItemBinding
+
+class LeaderboardItemActivity : AppCompatActivity() {
+    lateinit var binding: LeaderboardItemActivity
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
